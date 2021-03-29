d687295c115aaf04bd9c962bb1584b1531f46f76
josea <josea@DESKTOP-K9IFN4J.mshome.net>
2021-03-29 13:34:25 +0200

    Shout Completed

---
+++ src/main/java/acme/entities/shouts/Shout.java
@@ -0,0 +1,50 @@
+
+package acme.entities.shouts;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Shout extends DomainEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	@Past
+	protected Date				moment;
+
+	@NotBlank
+	protected String			author;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			text;
+
+	@URL
+	// This attribute is optional!
+	protected String			info;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+}
