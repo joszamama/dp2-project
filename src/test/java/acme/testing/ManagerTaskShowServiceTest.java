99ed4b5d1bfc2270fab69fb95ca8bcec5a504333
Adrián de las Heras Cózar <adrian.delasheras.cozar@gmail.com>
2021-05-17 02:29:23 +0200

    Added Manager Test Suite

---
+++ src/test/java/acme/testing/ManagerTaskShowServiceTest.java
@@ -0,0 +1,25 @@
+package acme.testing;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class ManagerTaskShowServiceTest extends AcmePlannerTest{
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/tasks/manager/show-tasks.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(1)
+	public void listAndShowTasks(final int recordIndex, final String title, final String description, final String executionStart, final String executionEnd, final String link, final String workload) {
+		super.signIn("manager1", "manager1");
+		super.clickOnMenu("Manager", "List my tasks");
+		
+		super.clickOnListingRecord(recordIndex);
+		
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("link", link);
+		super.checkInputBoxHasValue("executionStart", executionStart);
+		super.checkInputBoxHasValue("executionEnd", executionEnd);
+		super.checkInputBoxHasValue("workloadParsed", workload);
+	}
+}
