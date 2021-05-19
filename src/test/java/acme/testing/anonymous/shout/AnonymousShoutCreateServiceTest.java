5a23a008ba1c6c19f894268afc1ec2b16ab01946
estsvq <ezasdi@gmail.com>
2021-05-19 11:56:39 +0200

    Test esteban arreglados

--- src/test/java/acme/testing/anonymous/shout/AnonymousShoutCreateServiceTest.java
+++ src/test/java/acme/testing/anonymous/shout/AnonymousShoutCreateServiceTest.java
@@ -8,17 +8,36 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import acme.testing.AcmePlannerTest;
 
 public class AnonymousShoutCreateServiceTest extends AcmePlannerTest {
 
+//	@Override
+//	@BeforeAll
+//	public void beforeAll() {
+//		super.beforeAll();
+//
+//		super.setBaseCamp("http", "localhost", "8080", "/Acme-Planner", "/master/welcome", "?language=en&debug=true");
+//		super.setAutoPausing(true);
+//
+////		super.click(By.linkText("Anonymous"));
+////		super.submit(By.linkText("Create shout"));
+//	}
+	
 	@ParameterizedTest
 	@CsvFileSource(resources = "/anonymous/shout/positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	@Order(10)
-	public void positiveCreateShout(final String author, final String text, final String info) {
+	@Order(2)
+	public void positiveCreateShout(final String author,final String text,final String info) {
+		
+		super.driver.get("http://localhost:8080/Acme-Planner/master/welcome?language=en&debug=true");
+		final WebDriverWait wait = new WebDriverWait(super.driver, 10);
 		this.createShout(author, text, info);
+		wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Anonymous")));	
 		super.click(By.linkText("Anonymous"));
+		wait.until(ExpectedConditions.elementToBeClickable(By.linkText("List shouts")));	
 		super.click(By.linkText("List shouts"));
 		// assert super.exists(By.linkText("Account"));
 		super.click(By.xpath("//*[@id=\"list\"]/thead/tr/th[2]"));
@@ -42,18 +61,20 @@ public class AnonymousShoutCreateServiceTest extends AcmePlannerTest {
 	@CsvFileSource(resources = "/anonymous/shout/negative.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(1)
 	public void negativeSpamCreateShout(final String author, final String text, final String info) {
+		super.driver.get("http://localhost:8080/Acme-Planner/master/welcome?language=en&debug=true");
 		this.createShout(author, text, info);
 		super.checkErrorsExist();
 	}
 
 	protected void createShout(final String author, final String text, final String info) {
-		super.navigateHome();
-		super.clickAndGo(By.linkText("Anonymous"));
-		super.clickAndGo(By.linkText("Create shout"));
+		super.driver.get("http://localhost:8080/Acme-Planner/master/welcome?language=en&debug=true");
+		//super.navigateHome();
+		super.click(By.linkText("Anonymous"));
+		super.click(By.linkText("Create shout"));
 		super.fill(By.id("author"), author);
 		super.fill(By.id("text"), text);
 		super.fill(By.id("info"), info);
-		super.clickAndGo(By.className("btn-primary"));
+		super.click(By.className("btn-primary"));
 	}
 
 }
