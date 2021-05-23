47604e70a415688566b8603bc21802cc6e789776
José Antonio <joszamama@gmail.com>
2021-05-23 15:59:55 +0200

    organised juan tests

--- src/test/java/acme/testing/authenticated/task/AuthenticatedTaskListServiceTest.java
+++ src/test/java/acme/testing/authenticated/task/AuthenticatedTaskListServiceTest.java
@@ -1,5 +1,5 @@
 
-package acme.testing.authenticated;
+package acme.testing.authenticated.task;
 
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -7,40 +7,21 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 import acme.testing.AcmePlannerTest;
 
-public class AuthenticatedTest extends AcmePlannerTest {
+public class AuthenticatedTaskListServiceTest extends AcmePlannerTest {
 
-	@ParameterizedTest
-	@CsvFileSource(resources = "/tasks/authenticated/list-finished-public.csv", encoding = "utf-8", numLinesToSkip = 1)
-	@Order(10)
-	public void becomeManagerPositive() {
-		super.signUp("testusername", "testtest", "testname", "testsurname", "testemail@acme.com");
-		super.signIn("testusername", "testtest");
-		super.clickOnMenu("Account", "Become a manager");
-		
-		super.fillInputBoxIn("company", "test company 1");
-		super.fillInputBoxIn("sector", "testing department");
-		super.clickOnSubmitButton("Register");
-		
-		super.checkSimplePath("/master/welcome");
-		super.clickOnMenu("Account", "Manager data");
-		super.checkInputBoxHasValue("company", "test company 1");
-		super.checkInputBoxHasValue("sector", "testing department");
-	}
-	
 	@ParameterizedTest
 	@CsvFileSource(resources = "/tasks/authenticated/list-finished-public.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(1)
 	public void listFinishedPublic(final int recordIndex, final String title, final String description, final String executionStart, final String executionEnd, final String link, final String workload) {
 		super.signIn("administrator", "administrator");
 		super.clickOnMenu("Authenticated", "List finished public tasks");
-		
+
 		super.checkColumnHasValue(recordIndex, 0, title);
 		super.checkColumnHasValue(recordIndex, 1, executionStart);
 		super.checkColumnHasValue(recordIndex, 2, executionEnd);
 		super.checkColumnHasValue(recordIndex, 3, workload);
-		
+
 		super.clickOnListingRecord(recordIndex);
-		
 
 		super.checkInputBoxHasValue("title", title);
 		super.checkInputBoxHasValue("description", description);
@@ -48,7 +29,6 @@ public class AuthenticatedTest extends AcmePlannerTest {
 		super.checkInputBoxHasValue("executionStart", executionStart);
 		super.checkInputBoxHasValue("executionEnd", executionEnd);
 		super.checkInputBoxHasValue("workloadParsed", workload);
-		
 
 		this.signOut();
 	}
---
+++ src/test/java/acme/testing/authenticated/task/AuthenticatedTaskShowServiceTest.java
@@ -0,0 +1,8 @@
+
+package acme.testing.authenticated.task;
+
+import acme.testing.AcmePlannerTest;
+
+public class AuthenticatedTaskShowServiceTest extends AcmePlannerTest {
+
+}
