1ab1842c90db46ca0c0481a161852b3c8482ca25
carnuare <carnuare@gmail.com>
2021-05-25 18:22:48 +0200

    auth tests positive

--- src/test/java/acme/testing/authenticated/task/AuthenticatedTaskListServiceTest.java
+++ src/test/java/acme/testing/authenticated/task/AuthenticatedTaskListServiceTest.java
@@ -1,18 +1,30 @@
-
 package acme.testing.authenticated.task;
 
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import acme.testing.AcmePlannerTest;
 
 public class AuthenticatedTaskListServiceTest extends AcmePlannerTest {
+	
+	/**
+	 * First of all, we login as an administrator. Then, we access the authenticated list of tasks (finished and public)
+	 * 
+	 * 
+	 * @param title
+	 * @param description
+	 * @param link
+	 * @param executionStart
+	 * @param executionEnd
+	 * @param workload
+	 */
 
 	@ParameterizedTest
-	@CsvFileSource(resources = "/tasks/authenticated/list-finished-public.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/authenticated/task/list-finished-public.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void listFinishedPublic(final int recordIndex, final String title, final String description, final String executionStart, final String executionEnd, final String link, final String workload) {
+	public void positiveListFinishedPublic(final int recordIndex, final String title, final String executionStart, final String executionEnd, final String workload) {
 		super.signIn("administrator", "administrator");
 		super.clickOnMenu("Authenticated", "List finished public tasks");
 
@@ -21,15 +33,18 @@ public class AuthenticatedTaskListServiceTest extends AcmePlannerTest {
 		super.checkColumnHasValue(recordIndex, 2, executionEnd);
 		super.checkColumnHasValue(recordIndex, 3, workload);
 
-		super.clickOnListingRecord(recordIndex);
+		this.signOut();
+	}
+	/**
+	 * 
+	 * We check that we can't list finished and public tasks as an anonymous user.
+	 * 
+	 */
 
-		super.checkInputBoxHasValue("title", title);
-		super.checkInputBoxHasValue("description", description);
-		super.checkInputBoxHasValue("link", link);
-		super.checkInputBoxHasValue("executionStart", executionStart);
-		super.checkInputBoxHasValue("executionEnd", executionEnd);
-		super.checkInputBoxHasValue("workloadParsed", workload);
-
+	@Test
+	public void negativeTaskListing() {
+		super.driver.get("http://localhost:8080/Acme-Planner/authenticated/task/list");
+		this.checkPanicExists();
 		this.signOut();
 	}
 }
