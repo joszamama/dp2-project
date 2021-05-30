961ab7d46f15c9bc65b8b2f4cb13f346c6108b6d
José Antonio <joszamama@gmail.com>
2021-05-30 21:35:59 +0200

    UPDATED PERFORMANCE ANALYSIS

--- src/test/java/acme/testing/AcmePlannerTest.java
+++ src/test/java/acme/testing/AcmePlannerTest.java
@@ -17,11 +17,11 @@ public abstract class AcmePlannerTest extends AcmeTest {
 	@Override
 	@BeforeAll
 	public void beforeAll() {
+		super.setAutoPausing(false);
+		super.setHeadless(true);
 		super.beforeAll();
 
 		super.setBaseCamp("http", "localhost", "8080", "/Acme-Planner", "/master/welcome", "?language=en&debug=true");
-		super.setAutoPausing(false);
-		super.setHeadless(true);
 
 		this.navigateHome();
 		this.signIn("administrator", "administrator");
@@ -61,7 +61,7 @@ public abstract class AcmePlannerTest extends AcmeTest {
 		assert !StringHelper.isBlank(email);
 
 		super.navigateHome();
-		super.clickOnMenu("Sign up",null);
+		super.clickOnMenu("Sign up", null);
 		super.fillInputBoxIn("username", username);
 		super.fillInputBoxIn("password", password);
 		super.fillInputBoxIn("confirmation", password);
